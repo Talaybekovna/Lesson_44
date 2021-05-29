de28b39345ce70fcd351ba9295e4bdceedef1bfb
Kunduz <talaybekovna.kunduz@gmail.com>
2021-05-30 01:33:30 +0600

    project init

---
+++ app/src/main/java/kg/tutorialapp/l44_rv_setonclicklistener/MainActivity.kt
@@ -0,0 +1,11 @@
+package kg.tutorialapp.l44_rv_setonclicklistener
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
